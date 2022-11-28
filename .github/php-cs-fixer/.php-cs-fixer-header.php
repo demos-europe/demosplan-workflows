6e77b35849deb1c5328ec4f09c88b710cde240c3
Borys Shumyachkin <shumyachkin@demos-deutschland.de>
2022-11-28 13:28:56 +0100

    ci(refs T30148): move php-cs-fixer configs to workflow repo

---
+++ .github/php-cs-fixer/.php-cs-fixer-header.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__)
+    ->exclude([
+        'cache',
+        'lib',
+        'node_modules',
+        'services',
+        'var',
+        'vendor',
+    ]);
+
+$config = new PhpCsFixer\Config();
+
+$header = <<<HEADER
+This file is part of the package demosplan.
+
+(c) 2010-present DEMOS E-Partizipation GmbH, for more information see the license file.
+
+All rights reserved
+HEADER;
+
+$config
+    ->setRules(
+        [
+            'header_comment' => [
+                'header'       => $header,
+                'comment_type' => 'PHPDoc',
+                'location'     => 'after_declare_strict',
+            ],
+        ]
+    )
+    ->setCacheFile('.php-cs-fixer-header.cache')
+    ->setFinder($finder);
+
+return $config;
+
